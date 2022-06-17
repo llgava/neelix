6ec6bf44fbf285e3b3c9cb4fdb69e7341af9bebb
llgava <llgavamt@gmail.com>
2022-06-16 21:42:11 -0400

    remove: @NotNull annotation

--- src/main/java/net/llgava/commands/NeelixSubcommand.java
+++ src/main/java/net/llgava/commands/NeelixSubcommand.java
@@ -1,7 +1,6 @@
 package net.llgava.commands;
 
 
-import com.sun.istack.internal.NotNull;
 import lombok.Getter;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.entity.Player;
@@ -12,7 +11,7 @@ public abstract class NeelixSubcommand {
   @Getter private final String name;
   @Getter private final String permission;
 
-  public NeelixSubcommand(String name, @NotNull String permission) {
+  public NeelixSubcommand(String name, String permission) {
     this.name = name;
     this.permission = Objects.requireNonNull(permission, "Permission cannot be null, use empty string instead.");
   }
