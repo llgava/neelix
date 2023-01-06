c8f0a98e3fa687c21f6a2ef404d60b5ecb6cd639
Leonardo Luiz Gava <llgavamt@gmail.com>
2023-01-06 00:10:21 -0400

    remove: extra import

--- api/src/main/java/net/llgava/inventories/NeelixPaginatedNavigation.java
+++ api/src/main/java/net/llgava/inventories/NeelixPaginatedNavigation.java
@@ -5,7 +5,6 @@ import net.llgava.items.NextInventoryPageItem;
 import net.llgava.items.PreviousInventoryPageItem;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
